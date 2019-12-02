0515980101050286eb62213a597fb2b4f93d3ce8
asanaull <asanaull@localhost.localdomain>
2019-12-02 12:42:08 -0500

    added IP functionality

--- configure.sh
+++ configure.sh
@@ -3,6 +3,14 @@ echo "You must be the superuser to run this script" >&2
 exit 1
 fi
 
+DEVIP=(192 168 8 10)
+HOSTIP=(192 168 8 100)
+ROUTERIP=(192 168 8 1)
+DEVMAC="a25345b6fb5e"
+MASK=("FF" "FF" "FF" "00")
+
+
+
 UBP=$(ls /dev/ | grep "ttyUSB")
 
 if [ -z "$UBP" ]
@@ -10,9 +18,6 @@ then
       echo "FPGA not found"
       exit 1
 fi
-
-git clone https://github.com/asanaullah/ZipVersa-SSDP.git
-cd ZipVersa-SSDP
 
 
 dnf -y groupinstall "Development Tools" "Development Libraries"
@@ -85,6 +90,27 @@ dnf -y install ncurses-devel
 
 git clone https://github.com/ZipCPU/zipversa
 cd zipversa/
+
+sed -i "58d" sw/rv32/etcnet.h
+sed -i "58i #define	DEFAULTMAC	0x${DEVMAC}ul" sw/rv32/etcnet.h
+
+sed -i "66d" sw/rv32/etcnet.h
+sed -i "66i #define	DEFAULTIP	IPADDR(${DEVIP[0]},${DEVIP[1]},${DEVIP[2]},${DEVIP[3]})" sw/rv32/etcnet.h
+
+sed -i "72d" sw/rv32/etcnet.h
+sed -i "72i #define	LCLNETMASK 0x${MASK[0]}${MASK[1]}${MASK[2]}${MASK[3]}" sw/rv32/etcnet.h
+
+sed -i "77d" sw/rv32/etcnet.h
+sed -i "77i #define	DEFAULT_ROUTERIP	IPADDR(${ROUTERIP[0]},${ROUTERIP[1]},${ROUTERIP[2]},${ROUTERIP[3]})" sw/rv32/etcnet.h
+
+sed -i "148d" sw/host/udpsocket.cpp
+sed -i "148i 	getaddrinfo(\"${HOSTIP[0]}.${HOSTIP[1]}.${HOSTIP[2]}.${HOSTIP[3]}\", portstr, &hints, &res);" sw/host/udpsocket.cpp
+
+sed -i "208d" sw/host/testfft.cpp
+sed -i "208i 	UDPSOCKET *skt = new UDPSOCKET(\"${DEVIP[0]}.${DEVIP[1]}.${DEVIP[2]}.${DEVIP[3]}\");" sw/host/testfft.cpp
+
+
+
 \cp  ../Makefile .
 \cp -f ../custom_ops.S sw/rv32/
 \cp -f ../Makefile_rv32 sw/rv32/Makefile
